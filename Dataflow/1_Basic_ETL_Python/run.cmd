f82692ce07f8d2af14024095f6805f25f559a656
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-28 20:37:07 -0700

    my_pipeline.py - initital submit

---
+++ Dataflow/1_Basic_ETL_Python/run.cmd
@@ -0,0 +1,10 @@
+// 1) clone Git-repository
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+cd /home/jupyter/training-data-analyst/quests/dataflow_python/
+
+// 2) install dependencies
+pip install -r requirements.txt
+
+// 3) run the Dataflow job
+gcloud config set project qwiklabs-gcp-00-7dd053589081
+
