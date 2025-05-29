b2a95abfa908633046bbb81411f25c66516c702c
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-28 20:45:36 -0700

    events.json - add syntethic data

--- Dataflow/1_Basic_ETL_Python/run.cmd
+++ Dataflow/1_Basic_ETL_Python/run.cmd
@@ -2,9 +2,22 @@
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst
 cd /home/jupyter/training-data-analyst/quests/dataflow_python/
 
-// 2) install dependencies
-pip install -r requirements.txt
+// 2) create a virtual environment
+sudo apt-get update && sudo apt-get install -y python3-venv
+python3 -m venv df-env
 
-// 3) run the Dataflow job
-gcloud config set project qwiklabs-gcp-00-7dd053589081
+// 3) Install required packages 
+python3 -m pip install -q --upgrade pip setuptools wheel
+python3 -m pip install apache-beam[gcp]
 
+// 4) Finally, ensure that the Dataflow API is enabled
+gcloud services enable dataflow.googleapis.com
+
+// 5) Generate syntethic data
+export BASE_DIR=/home/jupyter/training-data-analyst/quests/dataflow_python/1_Basic_ETL/lab
+cd $BASE_DIR/../..
+source create_batch_sinks.sh
+bash generate_batch_events.sh
+head events.json
+
+// 6)
