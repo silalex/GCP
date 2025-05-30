d053ed60f7f5b3de4fe901f02d8a43cda22d5038
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-29 23:00:21 -0700

    run.cmd - run the pipeline (Part 2)

--- Dataflow/3_Batch_Analytics_Python/run.cmd
+++ Dataflow/3_Batch_Analytics_Python/run.cmd
@@ -60,4 +60,25 @@ python3 batch_user_traffic_pipeline.py \
 ##################### Part 2. Aggregating site traffic by minute #################
 ##################################################################################
 
-// 1) 
+// 1) modify "original" Python code according to the task requirements & run the pipeline
+
+// 2) run the pipeline
+
+export PROJECT_ID=$(gcloud config get-value project)
+export REGION=us-west1
+export BUCKET=gs://${PROJECT_ID}
+export PIPELINE_FOLDER=${BUCKET}
+export RUNNER=DataflowRunner
+export INPUT_PATH=${PIPELINE_FOLDER}/events.json
+export TABLE_NAME=${PROJECT_ID}:logs.minute_traffic
+
+cd $BASE_DIR
+python3 batch_minute_traffic_pipeline.py \
+--project=${PROJECT_ID} \
+--region=${REGION} \
+--staging_location=${PIPELINE_FOLDER}/staging \
+--temp_location=${PIPELINE_FOLDER}/temp \
+--runner=${RUNNER} \
+--input_path=${INPUT_PATH} \
+--table_name=${TABLE_NAME}
+
