5da920dc45a41c8f47898cbd30b70e06932fb5be
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-29 22:04:11 -0700

    run.cmd - setup preparation steps

---
+++ Dataflow/3_Batch_Analytics_Python/run.cmd
@@ -0,0 +1,18 @@
+// 1) Clone Github repo
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+cd /home/jupyter/training-data-analyst/quests/dataflow_python/
+
+// 2) Setting up virtual environment and dependencies
+sudo apt-get update && sudo apt-get install -y python3-venv
+
+# Create and activate virtual environment
+python3 -m venv df-env
+source df-env/bin/activate
+
+# install the packages you will need to execute your pipeline:
+python3 -m pip install -q --upgrade pip setuptools wheel
+python3 -m pip install apache-beam[gcp]
+
+# Ensure that the Dataflow API is enabled:
+gcloud services enable dataflow.googleapis.com
+
