6683f91cb27170084f13e78c878a195af0c8c342
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-31 14:33:21 -0700

    run.cmd - run the pipeline

--- Dataflow/2_Branching_Pipelines_Python/run.cmd
+++ Dataflow/2_Branching_Pipelines_Python/run.cmd
@@ -25,3 +25,31 @@ source generate_batch_events.sh
 # Change to the directory containing the practice version of the code
 cd $BASE_DIR
 
+
+
+// 6) Run the Dataflow pipeline
+
+# Set up environment variables
+export PROJECT_ID=$(gcloud config get-value project)
+export REGION=us-central1
+export BUCKET=gs://${PROJECT_ID}
+export COLDLINE_BUCKET=${BUCKET}-coldline
+export PIPELINE_FOLDER=${BUCKET}
+export RUNNER=DataflowRunner
+export INPUT_PATH=${PIPELINE_FOLDER}/events.json
+export OUTPUT_PATH=${PIPELINE_FOLDER}-coldline/pipeline_output
+export TABLE_NAME=${PROJECT_ID}:logs.logs_filtered
+
+cd $BASE_DIR
+python3 my_pipeline.py \
+--project=${PROJECT_ID} \
+--region=${REGION} \
+--stagingLocation=${PIPELINE_FOLDER}/staging \
+--tempLocation=${PIPELINE_FOLDER}/temp \
+--runner=${RUNNER} \
+--inputPath=${INPUT_PATH} \
+--outputPath=${OUTPUT_PATH} \
+--tableName=${TABLE_NAME}
+
+
+
