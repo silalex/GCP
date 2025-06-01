b565d113e9de88e1a0e051f6c6a8a5b7f7cc1889
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-31 19:52:25 -0700

    run.cmd - Prepare environment (a new Task)

---
+++ Dataflow/Arch_Java_Code/4_SQL_Batch_Analytics/run.cmd
@@ -0,0 +1,25 @@
+// 1) Prepare environment
+# Change directory into the lab
+cd 4_SQL_Batch_Analytics/labs
+
+# Download dependencies
+mvn clean dependency:resolve
+export BASE_DIR=$(pwd)
+
+
+// 2) Set up the data environment:
+# Create GCS buckets and BQ dataset
+cd $BASE_DIR/../..
+source create_batch_sinks.sh
+
+# Generate event dataflow
+source generate_batch_events.sh
+
+# Change to the directory containing the practice version of the code
+cd $BASE_DIR
+
+
+// 3) Ensure that the Data Catalog APIs is enabled:
+gcloud services enable datacatalog.googleapis.com
+
+
