b23f2a2e34ca009b3d7da0c5a06ae91bafc13432
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-31 18:27:15 -0700

    run.cmd - final pipeline run

--- Dataflow/7_Advanced_Streaming_Analytics/run.cmd
+++ Dataflow/7_Advanced_Streaming_Analytics/run.cmd
@@ -25,3 +25,32 @@ source create_streaming_sinks.sh
 # Change to the directory containing the practice version of the code
 cd $BASE_DIR
 
+
+// 6) Run the Dataflow pipeline
+
+export PROJECT_ID=$(gcloud config get-value project)
+export REGION=europe-west1
+export BUCKET=gs://${PROJECT_ID}
+export PIPELINE_FOLDER=${BUCKET}
+export RUNNER=DataflowRunner
+export PUBSUB_TOPIC=projects/${PROJECT_ID}/topics/my_topic
+export WINDOW_DURATION=60
+export ALLOWED_LATENESS=1
+export OUTPUT_TABLE_NAME=${PROJECT_ID}:logs.minute_traffic
+export DEADLETTER_BUCKET=${BUCKET}
+
+cd $BASE_DIR
+
+
+python3 streaming_minute_traffic_pipeline.py \
+--project=${PROJECT_ID} \
+--region=${REGION} \
+--staging_location=${PIPELINE_FOLDER}/staging \
+--temp_location=${PIPELINE_FOLDER}/temp \
+--runner=${RUNNER} \
+--input_topic=${PUBSUB_TOPIC} \
+--window_duration=${WINDOW_DURATION} \
+--allowed_lateness=${ALLOWED_LATENESS} \
+--table_name=${OUTPUT_TABLE_NAME} \
+--dead_letter_bucket=${DEADLETTER_BUCKET} \
+--allow_unsafe_triggers
