a83f16719a0767044fa05051e8072a9a62dfa4fe
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-05-31 20:34:13 -0700

    run.cmd - finally run the pipeline

--- Dataflow/Arch_Java_Code/4_SQL_Batch_Analytics/run.cmd
+++ Dataflow/Arch_Java_Code/4_SQL_Batch_Analytics/run.cmd
@@ -25,3 +25,30 @@ gcloud services enable datacatalog.googleapis.com
 
 // 4) ownload the dependencies through maven
 mvn clean dependency:resolve
+
+
+// 5) Run the Dataflow pipeline
+# Set up environment variables
+export PROJECT_ID=$(gcloud config get-value project)
+export REGION='us-central1'
+export BUCKET=gs://${PROJECT_ID}
+export PIPELINE_FOLDER=${BUCKET}
+export MAIN_CLASS_NAME=com.mypackage.pipeline.BatchUserTrafficSQLPipeline
+export RUNNER=DataflowRunner
+export INPUT_PATH=${PIPELINE_FOLDER}/events.json
+export AGGREGATE_TABLE_NAME=${PROJECT_ID}:logs.user_traffic
+export RAW_TABLE_NAME=${PROJECT_ID}:logs.raw
+
+cd $BASE_DIR
+mvn compile exec:java \
+-Dexec.mainClass=${MAIN_CLASS_NAME} \
+-Dexec.cleanupDaemonThreads=false \
+-Dexec.args=" \
+--project=${PROJECT_ID} \
+--region=${REGION} \
+--stagingLocation=${PIPELINE_FOLDER}/staging \
+--tempLocation=${PIPELINE_FOLDER}/temp \
+--runner=${RUNNER} \
+--inputPath=${INPUT_PATH} \
+--aggregateTableName=${AGGREGATE_TABLE_NAME} \
+--rawTableName=${RAW_TABLE_NAME}"
