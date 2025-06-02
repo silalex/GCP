7c14bcd33b4904094ebb50c63b7b79b4e267a288
ALEX SILIVIN <46965965+silalex@users.noreply.github.com>
2025-06-02 10:45:03 -0700

    run.cmd - epic fail for LAB's task (no Python code in Github)

--- Dataflow/8a_Batch_Testing_Pipeline/run.cmd
+++ Dataflow/8a_Batch_Testing_Pipeline/run.cmd
@@ -29,3 +29,7 @@ export PROJECT_ID=$(gcloud config get-value project)
 gcloud storage buckets create gs://$PROJECT_ID --location=US
 
 
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
+#########  no Python code in the Github-repo (only JAVA) #########
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
